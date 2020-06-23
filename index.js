99ceb715117cb83bdf84f897fb6c53cc09def896
TheGrEaTeStGaMeR <67283104+TheGrEaTeStGaMeR@users.noreply.github.com>
2020-06-23 15:42:19 +0530

    Bot

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ bot.login(process.env.token);
 
 bot.on('guildMemberAdd', member => {
 
-    const channel = member.guild.channels.cache.find(channel => channel.name == "🎊𝖂𝖊𝖑𝖈𝖔𝖒𝖊🎊");
+    const channel = member.guild.channels.cache.find(channel => channel.name == "general");
 
     channel.send(`Welcome to our Server ${member}.`);
 

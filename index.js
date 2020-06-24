e11376e97a0d227e056a879e22eaca28e8582e7c
TheGrEaTeStGaMeR <67283104+TheGrEaTeStGaMeR@users.noreply.github.com>
2020-06-24 13:20:00 +0530

    Bot

--- index.js
+++ index.js
@@ -83,11 +83,44 @@ bot.on('message', message => {
         case 'Hello':
             message.reply('Hello! How are you?');
             break;
-        
+
 
 
 
     }
 })
 
+var prefix = "+";
+client.on("message", message => {
 
+    if (message.content.startsWith(prefix + "dm")) {
+    if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+    let args = message.content.split(" ").slice(1);
+    var argresult = args.join(' '); 
+      message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
+     m.send(`${argresult}\n ${m}`);
+    })
+     message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` **MESSAGE SENT** `); 
+     message.delete(); 
+    };     
+    });
+    
+    
+    client.on('ready',()=>{
+        console.log(' Bot is online now!');
+        let statuses = [
+            "  STATUS HERE ",
+            "  STATUS HERE ",
+            "  STATUS HERE " // U can add another
+            
+        ]
+        setInterval(function(){
+                let status = statuses[Math.floor(Math.random() * statuses.length)];
+                client.user.setActivity(status, {type:"WATCHING"})
+        
+            }, 3000) //Seconds to Random
+    
+    
+    
+    });
+    
